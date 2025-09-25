353180c681d521bbbcf407c52edb22ea0cef51e2
Pui Yong Qing <puiyongqing@gmail.com>
2025-09-25 19:46:12 +0800

    carapare
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -5,75 +5,81 @@
 }:
 {
   home.shell.enableZshIntegration = true;
-  programs.carapace.enable = true;
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting = {
+  programs = {
+    carapace.enable = true;
+    nix-your-shell = {
       enable = true;
-      highlighters = [
-        "main"
-        "brackets"
-        "pattern"
-        "regexp"
-        "root"
-        "line"
+      nix-output-monitor.enable = true;
+    };
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [
+          "main"
+          "brackets"
+          "pattern"
+          "regexp"
+          "root"
+          "line"
+        ];
+      };
+      historySubstringSearch.enable = true;
+
+      history = {
+        ignoreDups = true;
+        save = 10000;
+        size = 10000;
+      };
+
+      oh-my-zsh = {
+        enable = true;
+      };
+
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./p10k-config;
+          file = "p10k.zsh";
+        }
       ];
-    };
-    historySubstringSearch.enable = true;
 
-    history = {
-      ignoreDups = true;
-      save = 10000;
-      size = 10000;
-    };
+      initContent = ''
+        bindkey "\eh" backward-word
+        bindkey "\ej" down-line-or-history
+        bindkey "\ek" up-line-or-history
+        bindkey "\el" forward-word
+        nix() {
+          case "$1" in
+            shell|develop|build)
+              nom "$@"
+              ;;
+            *)
+              command nix "$@"
+              ;;
+          esac
+        }
+      '';
 
-    oh-my-zsh = {
-      enable = true;
-    };
-
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource ./p10k-config;
-        file = "p10k.zsh";
-      }
-    ];
-
-    initContent = ''
-      bindkey "\eh" backward-word
-      bindkey "\ej" down-line-or-history
-      bindkey "\ek" up-line-or-history
-      bindkey "\el" forward-word
-      nix() {
-        case "$1" in
-          shell|develop|build)
-            nom "$@"
-            ;;
-          *)
-            command nix "$@"
-            ;;
-        esac
-      }
-    '';
-
-    shellAliases = {
-      sv = "sudo nvim";
-      v = "nvim";
-      c = "clear";
-      f = "c && fastfetch";
-      fr = "nh os switch";
-      ncg = "nh clean all";
-      cat = "bat";
-      man = "batman";
-      curl = "curlie";
-      nix-shell = "nom-shell";
-      nix-build = "nom-build";
+      shellAliases = {
+        sv = "sudo nvim";
+        v = "nvim";
+        c = "clear";
+        f = "c && fastfetch";
+        fr = "nh os switch";
+        ncg = "nh clean all";
+        cat = "bat";
+        man = "batman";
+        curl = "curlie";
+        nix-shell = "nom-shell";
+        nix-build = "nom-build";
+      };
     };
   };
 }
