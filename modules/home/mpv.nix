b380f717951b9864bef5c35bc76bc8dfa46e5aad
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-06 17:12:49 +0800

    arch wiki simply set auto
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -3,10 +3,7 @@
     enable = true;
     defaultProfiles = [ "gpu-hq" ];
     config = {
-      hwdec = "auto-safe";
-      vo = "gpu";
-      profile = "gpu-hq";
-      gpu-context = "wayland"; # On wayland only
+      hwdec = "auto";
     };
   };
 }
