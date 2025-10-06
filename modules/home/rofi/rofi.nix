3f134b72c9ef7bc776c0e92bb3e14978d2806eab
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-06 17:13:47 +0800

    reminder for changing rofi bg
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- modules/home/rofi/rofi.nix
+++ modules/home/rofi/rofi.nix
@@ -60,7 +60,7 @@
           "imagebox" = {
             padding = mkLiteral "20px";
             background-color = mkLiteral "transparent";
-            background-image = mkLiteral ''url("~/Pictures/Wallpapers/Rainnight.jpg", height)'';
+            background-image = mkLiteral ''url("~/Pictures/Wallpapers/Rainnight.jpg", height)''; # FIXME
             orientation = mkLiteral "vertical";
             children = map mkLiteral [
               "inputbar"
