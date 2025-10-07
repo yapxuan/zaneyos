c8de9509a72ea85316362c6ec5349c10c87cc7ec
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-08 06:49:25 +0800

    clean up
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- treefmt.nix
+++ treefmt.nix
@@ -2,10 +2,6 @@
   projectRootFile = "flake.nix";
   programs = {
     nixfmt.enable = true;
-    zig.enable = true;
-    gofmt.enable = true;
-    clang-format.enable = true;
-    taplo.enable = true;
-    rustfmt.enable = true;
+    shellcheck.enable = true;
   };
 }
