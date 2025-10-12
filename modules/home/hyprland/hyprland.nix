596d75b4a97eeddba43ebe9b01c527187f3c71ae
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-13 03:36:14 +0800

    rearrange startup order
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -47,14 +47,14 @@ in
     };
     settings = {
       exec-once = [
+        "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        "killall -q swaync;sleep && swaync"
         "wl-paste --type text --watch cliphist store # Stores only text data"
         "wl-paste --type image --watch cliphist store # Stores only image data"
-        "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user start hyprpolkitagent"
         "swww kill;sleep .5 && swww-daemon &"
         "killall -q waybar;sleep .5 && waybar"
-        "killall -q swaync;sleep .5 && swaync"
         "nm-applet --indicator"
         "pypr &"
         "clipse -listen"
