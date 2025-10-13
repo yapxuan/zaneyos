983eb2b8646fe1b037030157aa8af76ffb8ab251
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-14 06:12:14 +0800

    clean up extraConfig
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -1,15 +1,10 @@
 { pkgs, ... }:
 {
   gtk = {
+    colorScheme = "dark";
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
     };
   };
 }
