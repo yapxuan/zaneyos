22c95ca65acaf60b32ac839e0c3eeef98c0afc36
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-14 17:47:27 +0800

    ghostty: shell integration, quick terminal, fix background-blur, disable comfirm-close-surface
    Signed-off-by: Pui Yong Qing <puiyongqing@gmail.com>

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -11,7 +11,7 @@
       window-height = 32;
       window-width = 110;
       background-opacity = 0.95;
-      background-blur-radius = 60;
+      background-blur = 60;
       selection-background = "\#2d3f76";
       selection-foreground = "\#c8d3f5";
       cursor-style = "bar";
@@ -20,14 +20,19 @@
       font-family = "BerkeleyMono Nerd Font";
       title = "GhosTTY";
       wait-after-command = false;
+      confirm-close-surface = false;
       shell-integration = "detect";
       window-save-state = "always";
       gtk-single-instance = true;
       unfocused-split-opacity = 0.5;
-      quick-terminal-position = "center";
+      quick-terminal-position = "top";
+      quick-terminal-size = "110px,30px";
       quit-after-last-window-closed = true;
       quit-after-last-window-closed-delay = "5m";
-      shell-integration-features = "cursor,sudo";
+      shell-integration-features = "cursor,sudo,ssh-env,ssh-terminfo";
+      keybind = [
+        "global:super+t=toggle_quick_terminal"
+      ];
     };
   };
   systemd.user.services."app-com.mitchellh.ghostty" = {
